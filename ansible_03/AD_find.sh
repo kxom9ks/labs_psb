bef09267051240103d6fa879e479f78aa538e0d1
kxom9k <root@Server.home.local>
2026-03-26 17:15:02 +0700

    1

--- ansible_03/AD_find.sh
+++ ansible_03/AD_find.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 krb_path="$1"
-
-krb1="default_etypes = aes128-cts-hmac-sha1-96, aes256-cts-hmac-sha1-96"
-krb2="default_as_etypes = aes128-cts-hmac-sha1-96, aes256-cts-hmac-sha1-96"
-krb3="default_tgs_etypes = aes128-cts-hmac-sha1-96, aes256-cts-hmac-sha1-96"
+alg="aes128-cts-hmac-sha1-96, aes256-cts-hmac-sha1-96"
 
 sssd_cfg_path="$2"
-sssd1="cache_credentials = False"
+sssd1="cache_credentials"
 
 conf_d_path="$3"
 
 if [[ -e "$krb_path" ]]; then
 
-    if [[ $(grep -x "$krb1" "$krb_path") != "" && $(grep -x "$krb2" "$krb_path") != "" && $(grep -x "$krb3" "$krb_path") != "" ]]; then
+	krb1=$(grep -w "default_etypes" "$krb_path" | awk -F'=' '{print $2}' | xargs)
+	krb2=$(grep -w "default_as_etypes" "$krb_path" | awk -F'=' '{print  $2}' | xargs)
+	krb3=$(grep -w "default_tgs_etypes" "$krb_path" | awk -F'=' '{print $2}' | xargs)
+
+
+    if [[ "$krb1" == "$alg" && "$krb2" == "$alg" && "$krb3" == "$alg" ]]; then
         echo "Kerberos encrypton algorithms. Status - Ok."
     else
         echo "Kerberos encrypton algorithms. Status - Error. Solve the problem with config krb5.conf"
@@ -25,10 +27,10 @@ fi
 
 if [[ -e "$sssd_cfg_path" ]]; then
 
-    if [[ $(grep -xi "$sssd1" "$sssd_cfg_path") != "" ]]; then
+    if [[ $(grep -w "$sssd1" "$sssd_cfg_path" | awk -F'=' '{print $2}' | xargs ) == "false" ]]; then
         echo "Cache credentials not save. Status - Ok."
     else
-        echo "Status - Error. Cache error. Solve the problem with config sssd.conf"
+        echo "Error. Cache error. Solve the problem with config sssd.conf"
     fi
 
     if [[ $(grep "fallback_homedir" "$sssd_cfg_path" | awk -F'=' '{print $2}' | grep -v "@%d") != "" ]]; then
@@ -51,7 +53,7 @@ if [[ -z "$(ls -A $conf_d_path)" ]]; then
     if [[ $(grep -r "fallback_homedir" "$conf_d_path" | awk -F'=' '{print $2}' | grep -v "@%d") != "" ]]; then
         echo $(grep -r "fallback_homedir" "$conf_d_path" | awk -F'=' '{print $1 $2}' | grep -v "@%d") "Status - Error."
     elif [[ $(grep -r "fallback_homedir" "$conf_d_path") == "" ]]; then
-        echo "fallback_homedir in not exist, cause $conf_d_path is empty"
+        echo "fallback_homedir is not exist"
     else
         echo "fallback_homedir sssd.conf Status - Ok."
     fi
